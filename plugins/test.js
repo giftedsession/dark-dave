3016a1f4d5c098c933ff4bd8c1fda3aef29ebe66
giftedsession <oduorid606@gmail.com>
2025-07-08 12:26:43 +0300

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+let handler = async (m, { Gifted }) => {
+  await Gifted.sendMessage(m.chat, {
+    text: `𝐃𝐀𝐕𝐄-𝐗𝐌𝐃
+
+🔸 *Status:* Online ✅
+🔸 *Developer:* Gifted Dave
+🔸 *Mode:* ${Gifted.public ? "Public" : "Self"} 🔁`,
+    contextInfo: {
+      externalAdReply: {
+        title: "𝐃𝐀𝐕𝐄-𝐗𝐌𝐃 BOT",
+        body: "Powered by Gifted Dave",
+        thumbnailUrl: "https://files.catbox.moe/vr83h2.jpg",
+        sourceUrl: "https://github.com/gifteddevsmd/DAVE-XMD",
+        mediaType: 1,
+        renderLargerThumbnail: true
+      }
+    }
+  }, { quoted: m });
+};
+
+handler.help = ['tes', 'bot', 'status'];
+handler.tags = ['status'];
+handler.command = ['tes', 'bot', 'status'];
+
+module.exports = handler;
