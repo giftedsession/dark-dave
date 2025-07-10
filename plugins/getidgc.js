4c57c69cb7fa50f8356421f6fca474e3e85f4337
giftedsession <oduorid606@gmail.com>
2025-07-10 03:28:25 +0300

    Update getidgc.js

--- plugins/getidgc.js
+++ plugins/getidgc.js
@@ -1,28 +1,23 @@
-let handler = async (m, { Gifted }) => {
-    if (!m.isGroup) return m.reply('❌ This command only works in group chats.')
-
-    let groupId = `${m.chat}`
-    await Gifted.relayMessage(m.chat, {
-        requestPaymentMessage: {
-            currencyCodeIso4217: 'USD', // Use 'XXX' if you want a neutral/unknown currency
-            amount1000: 1000000000,
-            requestFrom: m.sender,
-            noteMessage: {
-                extendedTextMessage: {
-                    text: groupId,
-                    contextInfo: {
-                        externalAdReply: {
-                            showAdAttribution: true
-                        }
-                    }
-                }
-            }
-        }
-    }, {})
-}
-
+const axios = require('axios');
+let handler = async (m, { Gifted}) => {
+if (!m.isGroup) return m.reply(mess.group)
+let plr = `${m.chat}`
+await Gifted.relayMessage(m.chat,  {
+requestPaymentMessage: {
+currencyCodeIso4217: 'IDR',
+amount1000: 1000000000,
+requestFrom: m.sender,
+noteMessage: {
+extendedTextMessage: {
+text: plr,
+contextInfo: {
+externalAdReply: {
+showAdAttribution: true,
+}}}}}}, {})
+};
 handler.help = ['groupid']
 handler.tags = ['gcid']
 handler.command = ['getidgc']
 
-module.exports = handler
+
+module.exports = handler;
